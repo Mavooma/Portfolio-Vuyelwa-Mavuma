a8baa6f07a0234e43ce327a547bfe33962e1fc9a
Mavooma <mavumavuyelwa@gmail.com>
2024-06-09 15:00:10 +0200

    navlinks

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'flex') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'flex';
+    }
+});
